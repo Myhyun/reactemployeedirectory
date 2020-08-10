a3f17f60e38bb860fba38283027a1a91089408af
Myhyun <mattyihyun@gmail.com>
2020-08-10 09:28:33 -0700

    added results component

---
+++ src/components/Results.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Results(props) {
+    return (
+        <ul className="list-group">
+        {props.results.map((result) => (
+          <li className="list-group-item" key={result.id}>
+            <h1>
+                {result.employee_name}
+            </h1>
+            <p>
+                {result.employee_salary}
+                {result.employee_age}
+            </p>
+          </li>
+        ))}
+      </ul>
+    );
+}
+
+export default Results;
